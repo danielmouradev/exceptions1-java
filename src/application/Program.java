8a53c5b61bc8260b120a3506e20f5c433ca840f4
danielmouradev <danielmoura020713@gmail.com>
2025-05-28 21:18:14 -0300

    adicionado verificação das datas

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.entities.Reserva;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -15,7 +17,36 @@ public class Program {
         Integer numQuarto = sc.nextInt();
         System.out.print("Entre com a data de entrada: ");
         Date entrada = sdf.parse(sc.next());
+        System.out.print("Entre com a data de saída: ");
+        Date saida = sdf.parse(sc.next());
 
+        if (!saida.after(entrada)) {
+            System.out.println("Erro na reserva. A data de saída precisa ser posterior a da entrada!");
+        }else {
+            Reserva reserva = new Reserva(numQuarto, entrada, saida);
+            System.out.println("Reserva: " + reserva);
+
+            System.out.println();
+            System.out.println("ENTRE COM A DATA PARA ATUALIZAR A RESERVA");
+
+            System.out.print("Entre com a data de entrada: ");
+            entrada = sdf.parse(sc.next());
+            System.out.print("Entre com a data de saída: ");
+            saida = sdf.parse(sc.next());
+            reserva.atualizar(entrada, saida);
+
+            Date now = new Date();
+
+            if (entrada.before(now) || saida.before(now)) {
+                System.out.println("ERRO NA RESERVA. A DATA DE ATUALIZAÇÕES PRECISA SER FUTURA!");
+            } else if (!saida.after(entrada)) {
+                System.out.println("Erro na reserva. A data de saída precisa ser posterior a da entrada!");
+            } else {
+                reserva.atualizar(entrada, saida);
+                System.out.println("Reserva: " + reserva);
+
+            }
+        }
 
 
 
